e2ae77c5aea4be0efe7a955cb9adcb2702cf9101
Oksana Mironova <kq3hip13z@gmail.com>
2024-06-25 22:40:54 +0200

    The first part of the task

---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,14 @@
+import './style.css';
+
+export const Banner = () => (
+	<section id="menu" className="banner">
+		<div className="container">
+			<div className="banner__content">
+				<h1>Přijď to ochutnat!</h1>
+				<p>
+					Nabízíme kvalitní kávu, útulné prostředí a perfektní servis.
+				</p>
+			</div>
+		</div>
+	</section>
+);
--- src/pages/order.jsx
+++ src/pages/order.jsx
@@ -4,54 +4,50 @@ import './index.css';
 import './order.css';
 
 document.querySelector('#root').innerHTML = render(
-  <div className="page">
-    <div className="page">
-      <header>
-        <div className="container header__content">
-          <div className="site-logo"></div>
+	<div className="page">
+		<div className="page">
+			<header>
+				<div className="container header__content">
+					<div className="site-logo"></div>
 
-          <nav className="inline-nav">
-            <a href="/">Hlavní stránka</a>
-          </nav>
+					<nav className="inline-nav">
+						<a href="/index.html">Hlavní stránka</a>
+					</nav>
+				</div>
+			</header>
 
-        </div>
-      </header>
+			<main className="order">
+				<div className="container order__content">
+					<h1>Vaše objedávnka</h1>
+					<p className="empty-order">Zatím nemáte nic objednáno</p>
+					<div className="order__items">
+						<div className="order-item">
+							<img
+								src="/cups/espresso.png"
+								className="order-item__image"
+							/>
+							<div className="order-item__name">Espresso</div>
+						</div>
 
-      <main className="order">
-        <div className="container order__content">
-          <h1>Vaše objedávnka</h1>
-          <p className="empty-order">Zatím nemáte nic objednáno</p>
-          <div className="order__items">
-            <div className="order-item">
-              <img
-                src="/cups/espresso.png"
-                className="order-item__image"
-              />
-              <div className="order-item__name">
-                Espresso
-              </div>
-            </div>
+						<div className="order-item">
+							<img
+								src="/cups/doppio.png"
+								className="order-item__image"
+							/>
+							<div className="order-item__name">Doppio</div>
+						</div>
+					</div>
+				</div>
+			</main>
 
-            <div className="order-item">
-              <img
-                src="/cups/doppio.png"
-                className="order-item__image"
-              />
-              <div className="order-item__name">
-                Doppio
-              </div>
-            </div>
-          </div>
-        </div>
-      </main>
-
-      <footer>
-        <div className="container">
-          <div className="footer__content">
-            Café Lóra je tréningový projekt v rámci Czechitas kurzu JavaScript 2
-          </div>
-        </div>
-      </footer>
-    </div>
-  </div>
+			<footer>
+				<div className="container">
+					<div className="footer__content">
+						Café Lóra je tréningový projekt v rámci Czechitas kurzu
+						JavaScript 2
+					</div>
+				</div>
+			</footer>
+		</div>
+	</div>
 );
